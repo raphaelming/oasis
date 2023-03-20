3c3adb1c09f76d2970f34a4a75dc47fd917b55aa
raphaelming <spear131121@gmail.com>
2023-03-20 21:53:03 +0900

    project init

---
+++ src/main/java/com/raphaelming/oasis/controller/OasisApiController.java
@@ -0,0 +1,10 @@
+package com.raphaelming.oasis.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OasisApiController {
+
+
+}
---
+++ src/main/java/com/raphaelming/oasis/exception/BaseException.java
@@ -0,0 +1,4 @@
+package com.raphaelming.oasis.exception;
+
+public class BaseException {
+}
